dac353b25db0518d6f6ad5e2d0c392cee88c169a
EllaElogbamien <153170132+EllaLouis@users.noreply.github.com>
2025-01-27 00:09:37 -0700

    week-2

---
+++ app/week-2/student-Info.js
@@ -0,0 +1,16 @@
+import link from "next/link";
+
+export default function StudentInfo() {
+    return (
+        <div>
+            <h1>Student Information</h1>
+            <p>Name:Ella Elogbamien</p>
+            <p>
+                Github Repository:
+                <link href="https://github.com/EllaLouis/cprg306-assignments">
+                </link>
+            </p>
+        </div>
+    )
+}
+
