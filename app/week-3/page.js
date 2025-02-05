3aee75f410db9b4865eef52c3bcb61509360f4b8
EllaElogbamien <153170132+EllaLouis@users.noreply.github.com>
2025-02-04 21:32:30 -0700

    week-3

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -1,12 +1,11 @@
-import item from './item-list.js';
+import ItemList from './item-list.js';
 
 export default function Page() {
-    const Page = () => {
-        return (
-            <main className="bg-gray-100 min-h-screen flex flex-col items-center p-6">
-                <h1 className="text-3xl font-bold text-gray-800 mb-6">Shopping List</h1>
-                <ItemList />
-            </main>
-        );
-    };
-};
+    return (
+        <main className="bg-gray-100 min-h-screen flex flex-col items-center p-6">
+            <h1 className="text-3xl font-bold text-gray-800 mb-6">Shopping List</h1>
+            <ItemList />
+        </main>
+    );
+}
+
