f7fa87d3e6fae34ac94d1ed1c05dd0a38c746bc4
EllaElogbamien <153170132+EllaLouis@users.noreply.github.com>
2025-02-25 21:11:10 -0700

    week-5

--- app/week-5/page.js
+++ app/week-5/page.js
@@ -17,7 +17,11 @@ export default function Page() {
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-gray-800 text-white p-6">
-            <NewItem />
+            <NewItem
+                quantity={quantity}
+                setQuantity={setQuantity}
+            />
+
             <div className="text-2xl font-semibold my-4">{quantity}</div>
 
             <button
