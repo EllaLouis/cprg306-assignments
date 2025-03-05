3907ce73ff5265814294898ed514e0d7f59ec31c
EllaElogbamien <153170132+EllaLouis@users.noreply.github.com>
2025-03-05 01:05:02 -0700

    week-6

--- app/page.js
+++ app/page.js
@@ -9,6 +9,7 @@ export default function Home() {
         <Link href="/week-3"> Week 3 page</Link>
         <Link href="/week-4"> Week 4 page</Link>
         <Link href="/week-5"> Week 5 page</Link>
+        <Link href="/week-5"> Week 6 page</Link>
       </p>
     </div>
   );
---
+++ app/week-6/item.js
@@ -0,0 +1,16 @@
+export default function Item({ name, category }) {
+    return <li>{name} - {category}</li>;
+}
+;
+const Item = ({ name, quantity, category }) => {
+    return (
+        <li className="p-4 border-b border-gray-300 flex justify-left">
+            <div>
+                <p className="text-lg font-semibold">{name}</p>
+                <p className="text-sm text-gray-500">Category: {category}</p>
+            </div>
+            <span className="text-blue-600 font-medium">{quantity}</span>
+        </li>
+    );
+};
+
