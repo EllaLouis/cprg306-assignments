36affe00d6ae2e32b4c91f17fd8d822aeda5f1df
EllaElogbamien <153170132+EllaLouis@users.noreply.github.com>
2025-03-05 20:34:57 -0700

    week-6

--- app/page.js
+++ app/page.js
@@ -4,13 +4,14 @@ export default function Home() {
   return (
     <div>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <p>
-        <Link href="/week-2"> Week 2 page</Link>
-        <Link href="/week-3"> Week 3 page</Link>
-        <Link href="/week-4"> Week 4 page</Link>
-        <Link href="/week-5"> Week 5 page</Link>
-        <Link href="/week-5"> Week 6 page</Link>
-      </p>
+
+      <ul>
+        <li><Link href="/week-2">Week 2 page</Link></li>
+        <li><Link href="/week-3">Week 3 page</Link></li>
+        <li><Link href="/week-4">Week 4 page</Link></li>
+        <li><Link href="/week-5">Week 5 page</Link></li>
+        <li><Link href="/week-6">Week 6 page</Link></li>
+      </ul>
     </div>
   );
-};
+}
