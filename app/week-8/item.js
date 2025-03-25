3ee37d42092a1435947a3b205adb07ac6176fd89
EllaElogbamien <153170132+EllaLouis@users.noreply.github.com>
2025-03-25 14:18:05 -0600

    week-8

--- app/week-8/item.js
+++ app/week-8/item.js
@@ -3,9 +3,12 @@
 import React from "react";
 
 export default function Item({ id, name, quantity, category, onSelect }) {
+
+    const item = { id, name, quantity, category };
+
     return (
         <li
-            onClick={() => onSelect(id)}
+            onClick={() => onSelect(item)}
             className="p-2 border-b cursor-pointer hover:bg-gray-100"
         >
             <span className="font-semibold">{name}</span> - {quantity} ({category})
